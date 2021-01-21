eca4cc87a120ea0cb3a2322e3a17fea34a53bad7
tszczypx <tomaszx.szczypiorowski@intel.com>
2021-01-21 16:08:50 +0100

    Common Jenkins libs and scripts Adding scripts and libraries from pmem/pmemkv
    Modified directories and files structure, updated README, updated scripts.
    
    Signed-off-by: Tomasz Szczypiorowski <tomaszx.szczypiorowski@intel.com>

---
+++ jenkins/scripts/removeNamespaces.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2021, Intel Corporation
+
+# removeNamespaces.sh - Clear all existing namespaces.
+set -e
+
+echo "Clearing all existing namespaces"
+MOUNTPOINTS=$(lsblk | grep "pmem" | grep "/" | tr -s ' ' | cut -d ' ' -f 7)
+
+for m in $MOUNTPOINTS; do
+  sudo umount $m
+done
+
+namespace_names=$(ndctl list -X | jq -r '.[].dev')
+
+for n in $namespace_names; do
+	sudo ndctl clear-errors $n -v
+done
+
+sudo ndctl disable-namespace all
+sudo ndctl destroy-namespace all
