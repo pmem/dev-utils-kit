694f05c708cf23e5d4541ff58360c81d9a54f5fb
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2022-01-28 17:18:28 +0100

    docker: bump valgrind to the newest branch - pmem-3.18

--- docker/images/install-valgrind.sh
+++ docker/images/install-valgrind.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2016-2021, Intel Corporation
+# Copyright 2016-2022, Intel Corporation
 
 #
 # install-valgrind.sh - installs valgrind with pmemcheck
@@ -17,8 +17,8 @@ build_dir=$(mktemp -d -t valgrind-XXX)
 git clone https://github.com/pmem/valgrind.git ${build_dir}
 pushd ${build_dir}
 
-# pmem-3.17: Merge pull request #85 from lukaszstolarczuk/pmem-3.17; 16.08.2021
-git checkout ff6f0f125f8e1b1a2a8615f2b14efeaf135ad01b
+# pmem-3.18: memcheck: fix test addressable err exp; 21.01.2022
+git checkout 06f15d69237501852dd29883940e18da4179830a
 
 ./autogen.sh
 echo "### Valgrind autogen complete ###"
